4e7a0179f88a51a77755f8a3cfa7446c2ec8972b
Da Nguyen <nguyendanhda98@gmail.com>
2025-04-30 08:20:29 +0700

    fix: update package version to 0.1.83; enhance message transformation with detailed structure and improved data handling

--- src/transformers/MessageTransformer.ts
+++ src/transformers/MessageTransformer.ts
@@ -1,9 +1,29 @@
-import { Message } from "discord.js";
+import { Attachment, Message } from "discord.js";
 
 export async function messageToJson(message: Message): Promise<any> {
   const repliedMessage = await message.fetchReference().catch(() => null);
   return {
-    ...message,
+    channelId: message.channelId,
+    guildId: message.guildId,
+    id: message.id,
+    createdTimestamp: message.createdTimestamp,
+    content: message.content,
+    pinned: message.pinned,
+    embeds: message.embeds.map((embed) => embed.toJSON()),
+    attachments: message.attachments.map((attachment: Attachment) => ({
+      ...attachment,
+    })),
+    stickers: message.stickers.map((sticker) => ({
+      ...sticker,
+    })),
+    editedTimestamp: message.editedTimestamp,
+    reactions: message.reactions.cache.map((reaction) => ({
+      ...reaction,
+    })),
+    mentions: {
+      ...message.mentions,
+    },
+
     repliedMessage,
   };
 }
